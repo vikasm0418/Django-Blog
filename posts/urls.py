4675fbbada95e3a54d0f1d760ac656e23b4c88fa
vikas meena <vikasmeena0418@gmail.com>
2017-01-10 11:55:44 +0530

    Create urls.py

---
+++ posts/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    url(r'create/^$',views.post_create),
+    url(r'delete/^$',views.post_delete),
+    url(r'update/^$',views.post_update),
+    url(r'^$',views.post_list),
+    url(r'detail/^$',views.post_detail),
+]
